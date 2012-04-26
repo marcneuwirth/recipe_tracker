f2569badf605a4b8b9dedeee2b7c364bc9158e3f
marcneuwirth <marc.neuwirth@gmail.com>
2012-04-26 17:35:21 -0400

    fixed autocomplete order

--- recipeList/static/js/admin/script.js
+++ recipeList/static/js/admin/script.js
@@ -7,13 +7,22 @@ $(document).ready(function(){
 
 	if( $body.hasClass('recipeList-recipe')){
 		$.getJSON('/ingredients.json', function(json){
-			var ingredients = json.ingredients;
-
-			var autocomplete = function(){
-				$("#ingredient_set-group .dynamic-ingredient_set .field-name .vTextField").autocomplete({
-					source: ingredients
-				});
-			};
+			var ingredients = json.ingredients,
+				autocomplete = function(){
+					$("#ingredient_set-group .dynamic-ingredient_set .field-name .vTextField").autocomplete({
+						source: function( request, response ) {
+							var matcher = new RegExp( $.ui.autocomplete.escapeRegex( request.term ), "i" );
+							response( $.grep( ingredients, function( value ) {
+								value = value.label || value.value || value;
+								return matcher.test( value );
+							}).sort(function(a, b){
+								return a.indexOf(request.term) > b.indexOf(request.term);
+							}));
+						},
+						matchContains: false,
+						delay: 0
+					});
+				};
 
 			$('.add-row a').on('click', function(){
 				autocomplete();
